ae60a4de6066eab5076e57663a778282cc72836e
Jonathan Hernandez <jonhernandez@truecar.com>
2022-11-10 19:40:11 -0600

    Add <any> type to prevent "excessively deep" error

--- src/components/Containers.tsx
+++ src/components/Containers.tsx
@@ -1,21 +1,19 @@
 import tw from "tailwind-styled-components"
 
-const DefaultContainer = tw.main`
+const DefaultContainer = tw.main<any>`
   max-w-7xl
   mx-auto py-6 
   sm:px-6 
   lg:px-8
   p-2
 `
-const MatchesContainer = tw(DefaultContainer)`
+const MatchesContainer = tw(DefaultContainer)<any>`
   grid 
   grid-cols-1 
   md:grid-cols-2 
   gap-4
 `
 
-const PredictionsContainer = tw(MatchesContainer)`
-
-`
+const PredictionsContainer = tw(MatchesContainer)<any>``
 
 export { DefaultContainer, MatchesContainer, PredictionsContainer };
