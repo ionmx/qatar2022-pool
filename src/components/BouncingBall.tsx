a87c15413ea3c2b57917b61bd90497ff36d53549
Jonathan Hernandez <jonhernandez@truecar.com>
2022-11-18 00:23:44 -0600

    Bounce an emoji soccer ball

--- src/components/BouncingBall.tsx
+++ src/components/BouncingBall.tsx
@@ -1,9 +1,7 @@
 const BouncingBall = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen text-gray-900 text-2xl">
-      <svg className="animate-bounce w-12 h-12" viewBox="0 0 50 50">
-        <circle r="24" cx="24" cy="24" fill="#DDDDDD"></circle>
-      </svg>
+    <div className="flex items-center justify-center min-h-screen text-gray-900 opacity-25">
+      <span className="animate-bounce text-9xl">⚽️</span>
     </div>
   );
 };
