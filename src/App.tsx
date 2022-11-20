8dbd09245584540d885b7a122dff005951ae8fc6
Jonathan Hernandez <jonhernandez@truecar.com>
2022-11-20 11:23:30 -0600

    Columns

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function App() {
       <DefaultContainer>
         <div className="grid grid-cols-1 md:grid-cols-1 gap-4">
           <div>
-            <div className="grid grid-cols-2 md:grid-cols-1 gap-2">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
               {matches?.map((match: MatchProps) => {
                 matchDate = moment(match.date).format('YYYYMMDD');
                 if (matchDate === currentDate) {
